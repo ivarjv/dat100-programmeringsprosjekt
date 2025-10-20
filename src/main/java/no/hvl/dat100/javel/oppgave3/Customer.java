86f9280ecf8113a28f5e965f47572c231f0df34e
RagnhildSG <Ragnhild2002@hotmail.com>
2025-10-20 19:44:22 +0200

    Oppgave 3 men må finne kor e ska hente dataen fra

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -1,11 +1,10 @@
 package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
-    String names;
-    String emails;
-    int customer_ids;
-    PowerAgreementType agreements;
-
+    private String names;
+    private String emails;
+    private int customer_ids;
+    private PowerAgreementType agreements;
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
         names = name;
         emails = email;
@@ -13,8 +12,38 @@ public class Customer {
         agreements = agreement;
     }
 
+    //set
+    public void setNames(String names){
+    this.names = names;
+    }
+    public void setEmails(String emails){
+        this.emails = emails;
+    }
+    public void setCustomer_ids(int customer_ids){
+        this.customer_ids = customer_ids;
+    }
+    public void setAgreements(PowerAgreementType agreements){
+        this.agreements = agreements;
+    }
+    //get
+    public String getNames() {
+    return names;
+    }
+    public String getEmails() {
+        return emails;
+    }
+    public int getCustomer_ids() {
+        return customer_ids;
+    }
+
+    public PowerAgreementType getAgreements() {
+        return agreements;
+    }
+
     // TODO - getter/setter methods for all objectvariables
 
     // TODO - toString method
-
+    public String toString() {
+        return names + " " + emails + " " + customer_ids + " " + agreements;
+    }
 }
