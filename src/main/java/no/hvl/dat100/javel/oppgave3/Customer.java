28db065ef5ac6500f252abe877f354f48cff9056
RagnhildSG <Ragnhild2002@hotmail.com>
2025-10-20 18:30:42 +0200

    Oppgave 3 a og b

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -1,12 +1,16 @@
 package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
-
-    // TODO - object variables (attributes)
+    String names;
+    String emails;
+    int customer_ids;
+    PowerAgreementType agreements;
 
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
-
-        // TODO
+        names = name;
+        emails = email;
+        customer_ids = customer_id;
+        agreements = agreement;
     }
 
     // TODO - getter/setter methods for all objectvariables
